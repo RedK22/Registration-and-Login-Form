47e6a0c7f695afc95fe0efc0db5f246e961189b8
Red22K <ritwik22k@gmail.com>
2023-11-11 22:28:17 +0530

    More javascript functionality to be added.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// To randomize the backgrounds
+
 document.addEventListener("DOMContentLoaded", function () {
   var backgrounds = ["./img1.png", "./img2.png", "./img3.png", "./img.jpg"];
   var randomIndex = Math.floor(Math.random() * backgrounds.length);
