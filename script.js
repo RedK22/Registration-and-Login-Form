13edc694fdeb58f675aa2190e4f59cf49ed0005e
Red22K <ritwik22k@gmail.com>
2023-11-11 22:26:38 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var backgrounds = ["./img1.png", "./img2.png", "./img3.png", "./img.jpg"];
+  var randomIndex = Math.floor(Math.random() * backgrounds.length);
+  var selectedBackground = backgrounds[randomIndex];
+  document.body.style.backgroundImage = "url(" + selectedBackground + ")";
+});
