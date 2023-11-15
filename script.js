d1ffe376f09861986b88d5801074685063af95bf
Red22K <ritwik22k@gmail.com>
2023-11-16 01:12:06 +0530

    Fixed the type of Mobile Number in registration.

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // To randomize the backgrounds
 
-document.addEventListener("DOMContentLoaded", function () {
-  var backgrounds = ["./img1.png", "./img2.png", "./img3.png", "./img.jpg"];
-  var randomIndex = Math.floor(Math.random() * backgrounds.length);
-  var selectedBackground = backgrounds[randomIndex];
-  document.body.style.backgroundImage = "url(" + selectedBackground + ")";
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//   var backgrounds = ["./img1.png", "./img2.png", "./img3.png", "./img.jpg"];
+//   var randomIndex = Math.floor(Math.random() * backgrounds.length);
+//   var selectedBackground = backgrounds[randomIndex];
+//   document.body.style.backgroundImage = "url(" + selectedBackground + ")";
+// });
+
+//
